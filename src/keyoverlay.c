7a062f8c962b46e6d4a0656e38d1eb63a7a1cfcd
Mingyang Li <tslmy@outlook.com>
2026-06-13 13:14:16 -0700

    fixup! Create file for Keyboard Overlay program.

--- src/keyoverlay.c
+++ src/keyoverlay.c
@@ -17,16 +17,24 @@
  * still reach the console/applications as usual.
  */
 
+#include <ctype.h>
+#include <dirent.h>
 #include <errno.h>
+#include <fcntl.h>
 #include <getopt.h>
+#include <signal.h>
 #include <stdbool.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
+
 #include <linux/fb.h>
+#include <linux/input.h>
 #include <sys/ioctl.h>
 #include <sys/mman.h>
+#include <sys/stat.h>
 
 #include "font8x16.h"
 
